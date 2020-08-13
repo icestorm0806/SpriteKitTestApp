878729cfa740ce1c72666763d1353b15534c6694
Aaron Fulwider <icestorm0806@gmail.com>
2020-08-13 12:17:47 -0700

    Initial Commit

---
+++ SpritekitTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpritekitTestApp
+//
+//  Created by Aaron Fulwider on 8/7/20.
+//  Copyright © 2020 Aaron Fulwider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
